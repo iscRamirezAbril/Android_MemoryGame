814c3073b4fcdf789d4779877607f791078f294c
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2023-09-27 16:02:58 -0700

    Frontend de pantalla principal terminado. Pendiente el backend.

--- app/src/main/java/com/example/android_memorygame/MainActivity.kt
+++ app/src/main/java/com/example/android_memorygame/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.android_memorygame
 
+import android.app.ActionBar.LayoutParams
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main)
     }
 }
