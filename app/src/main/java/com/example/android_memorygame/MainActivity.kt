74fbcbdee35d3394587ccf8043758fc6252d6141
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2023-09-28 09:35:08 -0700

    Botón 1 funcional y corrección de errores.

--- app/src/main/java/com/example/android_memorygame/MainActivity.kt
+++ app/src/main/java/com/example/android_memorygame/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.example.android_memorygame
 
-import android.app.ActionBar.LayoutParams
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Window
 import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_main.*
 
+private const val TAG = "MainActivity";
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         setContentView(R.layout.activity_main)
+
+            imageButton1.setOnClickListener {
+            Log.i(TAG, "¡El botón ha sido presionado!")
+        }
     }
 }
